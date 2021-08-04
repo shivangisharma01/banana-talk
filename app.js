7b3f788832d76c8e81a7618d8f72eb8544a778db
Shivangi Sharma <sharmashivangi097@gmail.com>
2021-08-04 20:25:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput)
+function clickEventHandler() {
+    console.log("Clicked!");
+console.log("input" , txtInput.value)
+}
+btnTranslate.addEventListener("click", clickEventHandler)
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert box and prompt code together ->
+
+// var userName= prompt ("give me your name");
+// var welcomeMessage= ("You Entered!" + userName);
+// alert (welcomeMessage);
